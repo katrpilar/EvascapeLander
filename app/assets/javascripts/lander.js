abbebcfa2507554ae7bfd08118a27209f294277b
katrpilar <kbrinso@gmail.com>
2018-05-28 19:26:38 -0700

    completed page

---
+++ app/assets/javascripts/lander.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $('.parallax').parallax();
+   $('.materialboxed').materialbox();
+   $('.fixed-action-btn').floatingActionButton();
+   $('.slider').slider();
+
+});
+
