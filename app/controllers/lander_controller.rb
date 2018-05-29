5fd51d781afb0e2bf426ddaf725910a92c63484b
katrpilar <kbrinso@gmail.com>
2018-05-29 07:42:51 -0700

    Updated email signup

--- app/controllers/lander_controller.rb
+++ app/controllers/lander_controller.rb
@@ -1,4 +1,15 @@
 class LanderController < ApplicationController
+
 	def show
+	end
+
+	def subscribe
+		mailchimp = Mailchimp::API.new(MAILCHIMP-API-KEY)
+
+		mailchimp.lists.subscribe(MAILCHIMP-LIST-ID, 
+		                   { "email" => "awais545@gmail.com",
+		                     "euid" => "123",
+		                     "leid" => "123123"
+		                   })
 	end
 end
