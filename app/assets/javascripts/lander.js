2d43d6774f7c0d3260a40d97ffb65a4fcd391f6d
katrpilar <kbrinso@gmail.com>
2018-06-04 17:13:20 -0700

    design v2 ritual

--- app/assets/javascripts/lander.js
+++ app/assets/javascripts/lander.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
    $('.parallax').parallax();
    $('.materialboxed').materialbox();
    $('.fixed-action-btn').floatingActionButton();
-   $('.slider').slider();
+   $('.slider').slider({
+   	indicators:false,
+   	height: 400
+   });
+
+   $('.sidenav').sidenav();
+
 
 });
 
--- app/controllers/lander_controller.rb
+++ app/controllers/lander_controller.rb
@@ -3,13 +3,13 @@ class LanderController < ApplicationController
 	def show
 	end
 
-	def subscribe
-		mailchimp = Mailchimp::API.new(MAILCHIMP-API-KEY)
+	# def subscribe
+	# 	mailchimp = Mailchimp::API.new(MAILCHIMP-API-KEY)
 
-		mailchimp.lists.subscribe(MAILCHIMP-LIST-ID, 
-		                   { "email" => "awais545@gmail.com",
-		                     "euid" => "123",
-		                     "leid" => "123123"
-		                   })
-	end
+	# 	mailchimp.lists.subscribe(MAILCHIMP-LIST-ID, 
+	# 	                   { "email" => "awais545@gmail.com",
+	# 	                     "euid" => "123",
+	# 	                     "leid" => "123123"
+	# 	                   })
+	# end
 end
